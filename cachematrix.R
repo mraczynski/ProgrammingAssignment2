7b901b16bf51aa0a650d143afbee8143ed32b5fe
Michal Raczynski <m.raczynski@yahoo.com>
2014-12-22 01:19:38 +0100

    CachedMatrix solution done

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    #reset inverse matrix
+    inv <- NULL
+    
+    # define CachedMatrix methods
+    # to set matrix
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL 
+    }
+    # to get matrix
+    get <- function() x
+    #set inverse matrix (search parent environment)
+    setinv <- function(invx) inv <<- invx
+    #get inverse matrix
+    getinv <- function() inv
+    # return structured 
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+    
 }
 
 
@@ -12,4 +29,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    # try to get cached inversed matrix
+    inv <- x$getinv()
+    
+    # if cached inverse found display message 
+    # and return it without calculating
+    if(!is.null(inv)) {
+        message("found cached inversed matrix")
+        return(inv)
+    }
+    # if not found get matrix
+    invx <- x$get()
+    # calculate inverse
+    inv <- solve(invx, ...)
+    # set cache to calculated value
+    x$setinv(inv)
+    #return inverse matrix
+    x$getinv()
 }
